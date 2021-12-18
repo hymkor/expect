0989643dcbd16b9e28945268dbbad099935358a8
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-12-18 14:28:25 +0900

    Modify Makefile
    - `make package` do `make all`
    - `make all` outputs into bin/{386,amd64} instead of cmd/{386,amd64}

--- make.cmd
+++ make.cmd
@@ -14,15 +14,16 @@
     @exit /b
 
 :build
-    @if not exist "cmd"    mkdir "cmd"
-    @if not exist "cmd\%1" mkdir "cmd\%1"
+    @if not exist "bin"    mkdir "bin"
+    @if not exist "bin\%1" mkdir "bin\%1"
     set "GOARCH=%1"
-    go build -o cmd\%1\%NAME%.exe -ldflags "-s -w"
+    go build -o bin\%1\%NAME%.exe -ldflags "-s -w"
     @exit /b
 
 :"package"
-    set /P "VERSION=Version ? "
-    for %%I in (386 amd64) do zip -9j "%NAME%-%VERSION%-%%I.zip" "cmd\%%I\%NAME%.exe"
+    call :"all"
+    for /F %%I in ('git describe --tags') do set "VERSION=%%I"
+    for %%I in (386 amd64) do zip -9j "%NAME%-%VERSION%-%%I.zip" "bin\%%I\%NAME%.exe"
     @exit /b
 
 :"get"
