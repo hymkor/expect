af38d19a3d38b9f6fbdbe19805734e0e5b4f4fc5
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-12-28 21:03:37 +0900

    Add sleep and usleep test

--- test.lua
+++ test.lua
@@ -1,28 +1,77 @@
 do
     echo("*** timeout test ***")
-    local backup = timeout
-    timeout = 1
-    spawn("cmd.exe","/c","timeout 3 >nul & echo hoge")
-    local rc = expect("hoge")
-    if rc ~= -2 then
-        echo("--> [NG]")
-        os.exit(1)
+    do
+        local backup = timeout
+        timeout = 1
+        spawn("cmd.exe","/c","timeout 3 >nul & echo hoge")
+        local rc = expect("hoge")
+        if rc ~= -2 then
+            echo("--> [NG]")
+            os.exit(1)
+        end
+        echo("--> [OK]")
     end
-    echo("--> [OK]")
 
     echo("*** non-timeout test ***")
-    timeout = backup
-    rc = expect("hoge")
-    if rc ~= 0 then
-        echo("--> [NG]")
-        os.exit(1)
+    do
+        timeout = backup
+        local rc = expect("hoge")
+        if rc ~= 0 then
+            echo("--> [NG]")
+            os.exit(1)
+        end
+        echo("--> [OK]")
     end
-    echo("--> [OK]")
+
+    function testShot(word)
+        local screen = shot(2)
+        for i=1,#screen do
+            if string.find(screen[i],word,1,true) then
+                return true
+            end
+        end
+        return false
+    end
 
     echo("*** shot test ***")
-    local screen = shot(2)
-    if not string.find(screen[1],"shot") and not string.find(screen[2],"shot") then
-        echo("--> [NG]")
-        os.exit(1)
+    do
+        local screen = shot(2)
+        if not testShot("shot") then
+            echo("--> [NG]")
+            os.exit(1)
+        end
+        echo("--> [OK]")
+    end
+
+    echo("*** sleep test ***")
+    do
+        spawn("cmd.exe","/c","timeout 1 >nul & echo sleeptest")
+        if testShot("sleeptest") then
+            echo("--> [NG 1st/2]")
+            os.exit(1)
+        end
+        echo("--> [OK 1st/2]")
+        sleep(1)
+        if not testShot("sleeptest") then
+            echo("--> [NG 2nd/2]")
+            os.exit(1)
+        end
+        echo("--> [OK 2nd/2]")
+    end
+
+    echo("*** usleep test ***")
+    do
+        spawn("cmd.exe","/c","timeout 1 >nul & echo usleeptest")
+        if testShot("usleeptest") then
+            echo("--> [NG 1st/2]")
+            os.exit(1)
+        end
+        echo("--> [OK 1st/2]")
+        usleep(1000000)
+        if not testShot("usleeptest") then
+            echo("--> [NG 2nd/2]")
+            os.exit(1)
+        end
+        echo("--> [OK 2nd/2]")
     end
 end
